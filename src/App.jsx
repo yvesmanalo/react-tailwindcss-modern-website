1c553f07701f65d049fb07a1b3eb1069d43ce0f9
yvesmanalo <23yvesmanalo@gmail.com>
2026-03-07 21:26:15 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
+      <Navbar />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,83 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+  return (
+    <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-slate-950/20 backdrop-blur-sm">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between item-center h-14 sm:h-16 md:h-20">
+          <div className="flex items-center space-x-1 group cursor-pointer">
+            <div>
+              <img
+                src="/logo.png"
+                alt="CodeFlow"
+                className="w-6 h-6 sm:w-8 sm:h-8"
+              />
+            </div>
+            <span className="text-lg sm:text-xl md:text-2xl font-medium">
+              <span className="text-white">Code</span>
+              <span className="text-blue-400">Flow</span>
+            </span>
+          </div>
+
+          {/* Nav Links */}
+          <div className="hidden md:flex items-center space-x-6 lg:space-x-8">
+            <a
+              href="#features"
+              className="text-gray-300 hover:text-white text-sm lg:text-base"
+            >
+              Features
+            </a>
+            <a
+              href="#pricing"
+              className="text-gray-300 hover:text-white text-sm lg:text-base"
+            >
+              Pricing
+            </a>
+            <a
+              href="#testimonials"
+              className="text-gray-300 hover:text-white text-sm lg:text-base"
+            >
+              Testimonials
+            </a>
+          </div>
+          <button
+            className="md:hidden p-2 text-gray-300 hover:text-white"
+            onClick={() => setMobileMenuIsOpen((prev) => !prev)}
+          >
+            {mobileMenuIsOpen ? (
+              <X className="w-5 h-5 sm:w-6 sm:h-6" />
+            ) : (
+              <Menu className="w-5 h-5 sm:w-6 sm:h-6" />
+            )}
+          </button>
+        </div>
+      </div>
+      {mobileMenuIsOpen && (
+        <div className="md:hidden bg-slate-900/95 backdrop-blur-lg border-t border-slate-800  animate-in slide-in-from-top duration-300">
+          <div>
+            <a
+              href="#features"
+              className="text-gray-300 hover:text-white text-sm lg:text-base"
+            >
+              Features
+            </a>
+            <a
+              href="#pricing"
+              className="text-gray-300 hover:text-white text-sm lg:text-base"
+            >
+              Pricing
+            </a>
+            <a
+              href="#testimonials"
+              className="text-gray-300 hover:text-white text-sm lg:text-base"
+            >
+              Testimonials
+            </a>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+}
